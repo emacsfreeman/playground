4dafd654e75b87aa97d2a005ff8574689ca69e83
emacsfreeman <emacsfreeman@gmail.com>
2019-09-06 18:51:04 +0200

    Change asset data type from key,value to string

--- fabric-samples/chaincode/deadLine1/deadLine1.go
+++ fabric-samples/chaincode/deadLine1/deadLine1.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"crypto/x509"
 	"fmt"
-	"strings"
+	"reflect"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
@@ -12,20 +13,45 @@ import (
 type DeadLine1 struct {
 }
 
+type ClientIdentity interface {
+
+	// GetID returns the ID associated with the invoking identity.  This ID
+	// is guaranteed to be unique within the MSP.
+	GetID() (string, error)
+
+	// Return the MSP ID of the client
+	GetMSPID() (string, error)
+
+	// GetAttributeValue returns the value of the client's attribute named `attrName`.
+	// If the client possesses the attribute, `found` is true and `value` equals the
+	// value of the attribute.
+	// If the client does not possess the attribute, `found` is false and `value`
+	// equals "".
+	GetAttributeValue(attrName string) (value string, found bool, err error)
+
+	// AssertAttributeValue verifies that the client has the attribute named `attrName`
+	// with a value of `attrValue`; otherwise, an error is returned.
+	AssertAttributeValue(attrName, attrValue string) error
+
+	// GetX509Certificate returns the X509 certificate associated with the client,
+	// or nil if it was not identified by an X509 certificate.
+	GetX509Certificate() (*x509.Certificate, error)
+}
+
 // Init is called during chaincode instantiation to initialize any
 // data. Note that chaincode upgrade also calls this function to reset
 // or to migrate data.
 func (t *DeadLine1) Init(stub shim.ChaincodeStubInterface) peer.Response {
 	// Get the args from the transaction proposal
 	args := stub.GetStringArgs()
-	if len(args) != 2 {
-		return shim.Error("Incorrect arguments. Expecting a key and a value")
+	if reflect.TypeOf(args[0]) != reflect.TypeOf("string") {
+		return shim.Error("Incorrect argument. Expecting a string")
 	}
 
 	// Set up any variables or assets here by calling stub.PutState()
 
-	// We store the key and the value on the ledger
-	err := stub.PutState(args[0], []byte(args[1]))
+	// We store the string on the ledger
+	err := stub.PutState(args[0])
 	if err != nil {
 		return shim.Error(fmt.Sprintf("Failed to create asset: %s", args[0]))
 	}
@@ -36,53 +62,33 @@ func (t *DeadLine1) Init(stub shim.ChaincodeStubInterface) peer.Response {
 // either a 'get' or a 'set' on the asset created by Init function. The Set
 // method may create a new asset by specifying a new key-value pair.
 func (t *DeadLine1) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
-	function, args := stub.GetFunctionAndParameters()
-	switch function {
-	case "write":
-		return write(stub, args)
-	case "read":
-		return read(stub, args)
-	default:
-		return shim.Error("unknown function")
+	// Extract the function and args from the transaction proposal
+	fn, args := stub.GetFunctionAndParameters()
+
+	var result string
+	var err error
+	if fn == "set" {
+		result, err = set(stub, args)
+	} else { // assume 'get' even if fn is nil
+		result, err = get(stub, args)
 	}
-}
-
-// Write text by ID
-func write(stub shim.ChaincodeStubInterface, args []string) peer.Response {
-
-	if len(args) != 2 || len(args[0]) < 3 || len(args[1]) == 0 {
-		return shim.Error("Parameter Mismatch")
-	}
-	id := strings.ToLower(args[0])
-	txt := args[1]
-
-	if err := stub.PutState(id, []byte(txt)); err != nil {
+	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	return shim.Success(nil)
-}
-
-// Read text by ID
-func read(stub shim.ChaincodeStubInterface, args []string) peer.Response {
-
-	if len(args) != 1 {
-		return shim.Error("Parameter Mismatch")
-	}
-	id := strings.ToLower(args[0])
-
-	if value, err := stub.GetState(id); err == nil && value != nil {
-		return shim.Success(value)
-	}
-
-	return shim.Error("Not Found")
+	// Return the result as success payload
+	return shim.Success([]byte(result))
 }
 
 // Set stores the asset (both key and value) on the ledger. If the key exists,
 // it will override the value with the new one
 func set(stub shim.ChaincodeStubInterface, args []string) (string, error) {
-	if len(args) != 2 {
-		return "", fmt.Errorf("Incorrect arguments. Expecting a key and a value")
+	if reflect.TypeOf(args[0]) != reflect.TypeOf("string") {
+		return shim.Error("Incorrect argument. Expecting a string")
+	}
+
+	if stub.GetID() != "allowed" {
+		return "", fmt.Errorf("ID not allowed")
 	}
 
 	err := stub.PutState(args[0], []byte(args[1]))
@@ -92,13 +98,10 @@ func set(stub shim.ChaincodeStubInterface, args []string) (string, error) {
 	return args[1], nil
 }
 
-// Get returns the value of the specified asset key
+// Get returns the specified asset
 func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
-	if len(args) != 1 {
-		return "", fmt.Errorf("Incorrect arguments. Expecting a key")
-	}
 
-	value, err := stub.GetState(args[0])
+	asset, err := stub.GetState(args[0])
 	if err != nil {
 		return "", fmt.Errorf("Failed to get asset: %s with error: %s", args[0], err)
 	}
